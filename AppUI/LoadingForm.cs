f6696baeca4571285d25ccd3577136e60a1a3248
wilburomae <wilburomae@gmail.com>
2017-10-02 11:31:51 +0300

    Modify loading form

--- AppUI/LoadingForm.cs
+++ AppUI/LoadingForm.cs
@@ -6,7 +6,10 @@ namespace AppUI
     {
         private PictureBox pictureBox1;
         private Label label1;
-
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
@@ -17,26 +20,26 @@ namespace AppUI
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 31);
+            this.label1.Size = new System.Drawing.Size(55, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Please wait";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::AppUI.Properties.Resources.Loading;
-            this.pictureBox1.Location = new System.Drawing.Point(165, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(73, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(47, 28);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(16, 11);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // LoadingForm
             // 
-            this.ClientSize = new System.Drawing.Size(255, 49);
+            this.ClientSize = new System.Drawing.Size(101, 34);
+            this.ControlBox = false;
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -45,6 +48,8 @@ namespace AppUI
             this.Name = "LoadingForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
