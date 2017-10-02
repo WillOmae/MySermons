db81a3dd309674352c4053c919f9ac83e5a8f011
wilburomae <wilburomae@gmail.com>
2017-10-02 11:39:19 +0300

    Added close button

--- AppUI/LoadingForm.cs
+++ AppUI/LoadingForm.cs
@@ -5,41 +5,58 @@ namespace AppUI
     public class LoadingForm : Form
     {
         private PictureBox pictureBox1;
+        private Label lblClose;
         private Label label1;
         public LoadingForm()
         {
             InitializeComponent();
+            lblClose.Click += delegate
+            {
+                Close();
+            };
         }
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblClose = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(63, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Please wait";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::AppUI.Properties.Resources.Loading;
-            this.pictureBox1.Location = new System.Drawing.Point(73, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(89, 21);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(16, 11);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Arial Narrow", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.Location = new System.Drawing.Point(108, 32);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(27, 14);
+            this.lblClose.TabIndex = 2;
+            this.lblClose.Text = "close";
+            // 
             // LoadingForm
             // 
-            this.ClientSize = new System.Drawing.Size(101, 34);
+            this.ClientSize = new System.Drawing.Size(147, 55);
             this.ControlBox = false;
+            this.Controls.Add(this.lblClose);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
